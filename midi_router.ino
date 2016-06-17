ef0d14576284838039474778125f49c3e50a1557
David DeGraw <ddegraw@simplifile.com>
2016-06-17 15:22:00 -0600

    Refactor a bit more

--- midi_router.ino
+++ midi_router.ino
@@ -6,17 +6,24 @@
 #define RIGHT_HALF_CURSOR (uint8_t)1
 #define DISPLAY_WIDTH_IN_CHARS 16
 #define DISPLAY_HEIGHT_IN_CHARS 2
+#define TOP_ROW 0
+#define BOTTOM_ROW 1
+#define PORT_1_ROW
+#define PORT_2_ROW
+#define PORT_3_ROW
+#define PORT_4_ROW
+#define COLUMN_SPACE 4
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 // custom chars generated from: https://omerk.github.io/lcdchargen/
 byte leftDown[8] = {
-  0b10000,
-	0b10000,
-	0b10000,
-	0b11000,
-	0b01100,
-	0b00110,
+  0b00001,
+	0b00001,
+	0b00001,
+	0b00001,
+	0b01001,
+	0b00101,
 	0b00011,
 	0b00001
 };
@@ -33,44 +40,59 @@ byte rightDown[8] = {
 };
 
 int column = 1;
-bool button_flag = TRUE;
-bool button_value = TRUE;
+bool button_flag = true;
+bool button_value = true;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   lcd.createChar(0, leftDown);
   lcd.createChar(1, rightDown);
   lcd.begin(DISPLAY_WIDTH_IN_CHARS, DISPLAY_HEIGHT_IN_CHARS);
-  lcd.setCursor(column, 0);
+  lcd.setCursor(column, TOP_ROW);
   lcd.noAutoscroll();
   lcd.write(LEFT_HALF_CURSOR);
   lcd.write(RIGHT_HALF_CURSOR);
   Serial.begin(9600);
 }
 
-void loop() {
-  button_value = digitalRead(BUTTON_PIN);
-  if (!digitalRead(BUTTON_PIN) && !button_flag) {
-    // delay to debounce the input
+bool digitalReadDebounced(int pin) {
+  if (digitalRead(pin)) {
     delay(5);
-    if (!digitalRead(BUTTON_PIN)){
-      button_flag = TRUE;             // set the button flag to only change the cursor once per press
-      lcd.setCursor(column,0);
-      lcd.print(SPACER);              // erase the cursor
-      column += 4;                    // jump to the next space for the cursor
-      if (column >= 15) {             // if the cursor is as far right as it can be
-        lcd.print(SPACER);
-        column = 1;
-      }
-      lcd.setCursor(column,0);
-      lcd.write(LEFT_HALF_CURSOR);    // display the cursor
-      lcd.write(RIGHT_HALF_CURSOR);
+    if (digitalRead(pin)) {
+      return true;
     }
   }
-  else if (digitalRead(BUTTON_PIN)) {
-    button_flag = FALSE;
+  return false;
+}
+
+void drawCursor() {
+  lcd.write(LEFT_HALF_CURSOR);    // display the cursor
+  lcd.write(RIGHT_HALF_CURSOR);
+}
+
+void eraseCursor() {
+  lcd.print(SPACER);              // erase the cursor
+}
+
+void loop() {
+  button_value = digitalReadDebounced(BUTTON_PIN);
+  
+  if (!button_value && !button_flag) {
+    button_flag = true;             // set the button flag to only change the cursor once per press
+    lcd.setCursor(column, TOP_ROW);
+    eraseCursor();
+    column += COLUMN_SPACE;                    // jump to the next space for the cursor
+    if (column >= DISPLAY_WIDTH_IN_CHARS) {             // if the cursor is as far right as it can be
+      eraseCursor();
+      column = 1;                   // reset the column to the first position
+    }
+    lcd.setCursor(column, TOP_ROW);
+    drawCursor();
   }
-  lcd.setCursor(0, 1);
+  else if (button_value) {
+    button_flag = false;
+  }
+  lcd.setCursor(0, BOTTOM_ROW);
   lcd.print(millis() / 1000);
 }
 
