d3486cd8fe6facd3978ad57d21bc3b349e11ea5d
David DeGraw <ddegraw@simplifile.com>
2016-06-24 16:18:35 -0600

    Add mode switching, channel select, etc

--- midi_router.ino
+++ midi_router.ino
@@ -15,6 +15,10 @@
 #define COLUMN_SPACE 4
 #define MIN_CHANNEL 1
 #define MAX_CHANNEL 16
+#define MODE_CHANNEL 0
+#define MODE_VOLUME 1
+#define MODE_PITCH 2
+#define MODE_VOID -1
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -45,6 +49,11 @@ int column = 1;
 bool button_flag = true;
 bool button_value = true;
 int counter = 0;
+int mapped = 0;
+int oldVal = 0;
+int mode = MODE_VOID;
+int tempMode = MODE_VOID;
+unsigned long time;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -55,6 +64,13 @@ void setup() {
   lcd.noAutoscroll();
   lcd.write(LEFT_HALF_CURSOR);
   lcd.write(RIGHT_HALF_CURSOR);
+  mapped = map(analogRead(0), 0, 1015, 1, 16);
+  oldVal = mapped;
+  
+  // for (int i = 1; i < 5; i++) {
+  //   incrementPort(i, 1);
+  // }
+  
   Serial.begin(9600);
 }
 
@@ -77,7 +93,7 @@ void eraseCursor() {
   lcd.print(SPACER);              // erase the cursor
 }
 
-void incrementPort(int port_number) {
+void incrementPort(int port_number, int value) {
   int col = 0;
   switch (port_number) {
     case 1:
@@ -95,20 +111,19 @@ void incrementPort(int port_number) {
     }
     
     lcd.setCursor(col, BOTTOM_ROW);
-    if (counter > MAX_CHANNEL) {
-      eraseCursor();
-      lcd.setCursor(col, BOTTOM_ROW);
-      counter = MIN_CHANNEL;
+    eraseCursor();
+    lcd.setCursor(col, BOTTOM_ROW);
+
+    if (value) {
+      lcd.print(value);
     }
-    
-    lcd.print(counter);
-      
 }
 
-void loop() {
+void cycleCursor() {
   button_value = digitalReadDebounced(BUTTON_PIN);
   
   if (!button_value && !button_flag) {
+    updateTime();
     counter++;
     button_flag = true;             // set the button flag to only change the cursor once per press
     lcd.setCursor(column, TOP_ROW);
@@ -124,13 +139,98 @@ void loop() {
   else if (button_value) {
     button_flag = false;
   }
-  
-  // incrementPort(1);
-  // incrementPort(2);
-  incrementPort(3);
-  // incrementPort(4);
-
 }
+
+void changeChannel() {
+  mapped = map(analogRead(0), 0, 1015, 0, 16);
+  if (mapped != oldVal) {
+    updateTime();
+    if (column == PORT_1_COLUMN){
+      incrementPort(1, mapped);
+    }
+    else if (column == PORT_2_COLUMN) {
+      incrementPort(2, mapped);
+    }
+    else if (column == PORT_3_COLUMN) {
+      incrementPort(3, mapped);
+    }
+    else if (column == PORT_4_COLUMN) {
+      incrementPort(4, mapped);
+    }
+  }
+  oldVal = mapped;
+}
+
+void selectMode() {
+  mapped = map(analogRead(0), 0, 1015, 0, 2);
+  if (mapped != oldVal) {
+    lcd.setCursor(0,0);
+    lcd.print("               ");
+    switch (mapped) {
+      case 0:
+        tempMode = MODE_CHANNEL;
+        lcd.setCursor(0,0);
+        lcd.print("Channel");
+        break;
+      case 1:
+        tempMode = MODE_VOLUME;
+        lcd.setCursor(0,0);
+        lcd.print("Volume");
+        break;
+      case 2:
+        tempMode = MODE_PITCH;
+        lcd.setCursor(0,0);
+        lcd.print("Pitch Offset");
+        break;
+    }
+  }
+  oldVal = mapped;
+}
+
+void updateTime() {
+  time = millis();
+}
+
+void checkTimeout() {
+  if ((millis() - time) > 2000) {
+    clearScreen();
+    mode = MODE_VOID;
+  }
+}
+
+void setMode() {
+  button_value = digitalReadDebounced(BUTTON_PIN);
+  
+  if (!button_value && !button_flag) {
+    mapped = oldVal;
+    clearScreen();
+    mode = tempMode;
+  }
+  else if (button_value) {
+    button_flag = false;
+  }
+}
+
+void clearScreen() {
+  lcd.setCursor(0,0);
+  lcd.print("                ");
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+}
+
+void loop() {
+  if (mode == MODE_VOID) {
+    selectMode();
+    setMode();
+  }
+  else if (mode == MODE_CHANNEL) {
+    cycleCursor();
+    changeChannel();
+    checkTimeout();
+  }
+}
+
+
 
 
 
